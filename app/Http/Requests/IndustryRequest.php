d808e598f1b4f4375ce4a934aeaad7720152283d
aya124 <aya12496@gmail.com>
2021-03-01 12:11:12 +0700

    Update kecil

---
+++ app/Http/Requests/IndustryRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class IndustryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'address' => 'required|max:255',
+            'city' => 'required||max:255',
+            'phone' => 'integer|max:14',
+            'detail' => 'max:255',
+        ];
+    }
+    public function messages()
+    {
+        return[
+            'name.required'    => 'Kolom nama wajib diisi',
+            'name.string'      => 'Kolom nama berisi teks',
+            'name.max'         => 'Kolom nama maks 255 karakter',
+            'address.required' => 'Kolom alamat wajib diisi',
+            'address.max'      => 'Kolom alamat maks 255 karakter',
+            'city.required'    => 'Kolom kota wajib diisi',
+            'city.max'         => 'Kolom kota maks 255 karakter',
+            'phone.integer'    => 'Kolom no telepon berisi angka',
+            'phone.max'        => 'Kolom no telepon maks 14 karakter',
+            'detail'           => 'Kolom detail maks 255 karakter',
+        ];
+    }
+}
