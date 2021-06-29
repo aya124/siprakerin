ec3fd7e722d98c33a491a7500a5321eac525b793
aya124 <aya12496@gmail.com>
2021-06-29 08:39:20 +0700

    Edit permission view

--- resources/views/permission/index.blade.php
+++ resources/views/permission/index.blade.php
@@ -13,46 +13,46 @@
 @endif
 
 <div class="box">
-    <div class="box-header">
-        <h3 class="box-title">Permission Management</h3>
-    </div>
-    <!-- /.box-header -->
-    <div class="box-body">
-        <table id="tab_data" class="table table-bordered table-striped">
-          <thead>
-            <tr>
-              <th>Permission</th>
-              <th></th>
-            </tr>
-          </thead>
-        </table>
-    </div>
-    <!-- /.box-body -->
+  <div class="box-header">
+    <h3 class="box-title">Permission Management</h3>
+  </div>
+  <!-- /.box-header -->
+  <div class="box-body">
+    <table id="tab_data" class="table table-bordered table-striped">
+      <thead>
+        <tr>
+          <th>Permission</th>
+          <th>Aksi</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+  <!-- /.box-body -->
 </div>
 <!-- /.box -->
 
 <div id="createModal" class="modal fade" role="dialog">
-    <div class="modal-dialog">
-		<div class="modal-content">
+  <div class="modal-dialog">
+	  <div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal">&times;</button>
-				<h4 class="modal-title"></h4>
+				<h3 class="modal-title"></h3>
 			</div>
 			<div class="modal-body">
-                <span id="form_result"></span>
+        <span id="form_result"></span>
 				<form method="post" id="add" class="form-horizontal" enctype="multipart/form-data">
-					@csrf
-					<div class="form-group">
-						<label class="control-label col-md-4" >Permission <small class="text-danger">*</small> </label>
-						<div class="col-md-8">
-							<input type="text" name="name" id="name" class="form-control" />
-						</div>
-                    </div><br />
-					<div class="form-group" align="center">
-						<input type="hidden" name="action" id="action" />
-						<input type="hidden" name="hidden_id" id="hidden_id" />
-						<input type="submit" name="action_button" id="action_button" class="btn btn-primary" value="submit" />
-					</div>
+				@csrf
+				<div class="form-group">
+					<label class="control-label col-md-4" >Permission <small class="text-danger">*</small> </label>
+				<div class="col-md-8">
+					<input type="text" name="name" id="name" class="form-control" />
+				</div>
+        </div><br />
+				<div class="form-group" align="center">
+					<input type="hidden" name="action" id="action" />
+					<input type="hidden" name="hidden_id" id="hidden_id" />
+					<input type="submit" name="action_button" id="action_button" class="btn btn-primary" value="submit" />
+				</div>
 				</form>
 			</div>
 		</div>
@@ -60,21 +60,21 @@
 </div>
 
 <div id="confirmModal" class="modal fade" role="dialog">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h2 class="modal-title">Hapus data</h2>
-            </div>
-            <div class="modal-body">
-                <h4 align="center" style="margin:0;">Apakah anda yakin ingin menghapus data ini?</h4>
-            </div>
-            <div class="modal-footer">
-             <button type="button" name="ok_button" id="ok_button" class="btn btn-danger">OK</button>
-                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-            </div>
-        </div>
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h3 class="modal-title">Hapus data</h3>
+      </div>
+      <div class="modal-body">
+        <h4 align="center" style="margin:0;">Apakah anda yakin ingin menghapus data ini?</h4>
+      </div>
+      <div class="modal-footer">
+        <button type="button" name="ok_button" id="ok_button" class="btn btn-danger">OK</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+      </div>
     </div>
+  </div>
 </div>
 @stop
 
@@ -94,148 +94,143 @@
         url: "{{ route('permission.index') }}",
       },
       columns:[
-        {
+      {
 		    data: 'name',
 		    name: 'name',
-        },
-        {
-            data: 'action',
+      },
+      {
+        data: 'action',
 		    name: 'action',
 		    orderable: false
       }
-      ]
-    });
+    ]
+  });
 
-    $('#btn_add').click(function(){
-      $('.notifError').remove();
-      $('#createModal').modal('show');
-      $('#name').val("");
-      $('#form_result').hide();
-      $('#createModal .modal-title').text("Tambah Permission");
-      $('#action').val("tambah");
-    });
+  $('#btn_add').click(function(){
+    $('.notifError').remove();
+    $('#createModal').modal('show');
+    $('#name').val("");
+    $('#form_result').hide();
+    $('#createModal .modal-title').text("Tambah Permission");
+    $('#action').val("tambah");
+  });
 
-    var id_table;
-    var html = '';
-    $(document).on('click','.edit',function(){
-        id_table = $(this).attr('id');
-        $('#action').val("edit");
-        $('.notifError').remove();
-        $('#createModal').modal('show');
-        $('#form_result').hide();
-        $('#createModal .modal-title').text("Edit permission");
-        $.ajax({
-          url:"/admin/permission/"+id_table+"/edit",
-          dataType:"json",
-          success:function(html){
-              $('#name').val(html.data.name);
-              $('#hidden_id').val(html.data.id);
-              console.log($('#hidden_id').val(),$('#action').val());
-            }
-        });
+  var id_table;
+  var html = '';
+  $(document).on('click','.edit',function(){
+    id_table = $(this).attr('id');
+    $('#action').val("edit");
+    $('.notifError').remove();
+    $('#createModal').modal('show');
+    $('#form_result').hide();
+    $('#createModal .modal-title').text("Edit Permission");
+    $.ajax({
+      url:"/admin/permission/"+id_table+"/edit",
+      dataType:"json",
+      success:function(html){
+        $('#name').val(html.data.name);
+        $('#hidden_id').val(html.data.id);
+        console.log($('#hidden_id').val(),$('#action').val());
+      }
     });
+  });
 
-    $(document).on('click','.delete',function(){
-      id_table = $(this).attr('id');
-      $('#confirmModal').modal('show');
-      $('#ok_button').text('OK');
-    });
+  $(document).on('click','.delete',function(){
+    id_table = $(this).attr('id');
+    $('#confirmModal').modal('show');
+    $('#ok_button').text('OK');
+  });
 
-    $('#ok_button').click(function(){
-      console.log (id_table);
-      $.ajax({
-        url:"permission/destroy/"+id_table,
-        beforeSend:function(){
-            $('#ok_button').text('Deleting...');
-          },
-          success:function(data) {
-            setTimeout(function(){
-                $('#confirmModal').modal('hide');
-                $('#tab_data').DataTable().ajax.reload();
-            }, 2000);
-            toastr.success('Permission berhasil dihapus!', 'Success', {timeOut: 5000});
-          }
+  $('#ok_button').click(function(){
+    console.log (id_table);
+    $.ajax({
+      url:"permission/destroy/"+id_table,
+      beforeSend:function(){
+        $('#ok_button').text('Deleting...');
+      },
+      success:function(data) {
+        setTimeout(function(){
+          $('#confirmModal').modal('hide');
+          $('#tab_data').DataTable().ajax.reload();
+        }, 2000);
+        toastr.success('Permission berhasil dihapus!', 'Success', {timeOut: 5000});
+      }
 		})
 	});
 
-    $('#add').on('submit',function(event){
-      $('.notifError').remove();
-      event.preventDefault();
-      if($('#action').val() == 'tambah'){
-        $.ajax({
-          url:"{{route('permission.store')}}",
-          method:"POST",
-          contentType: false,
-          cache:false,
-          processData: false,
-          dataType:"json",
-          data: new FormData(this),
-          success:function(data) {
-            $('#form_result').hide();
-            if(data.success) {
-              html = '<div class="alert alert-success">' + data.success + '</div>';
-              setTimeout(function(){
-                $('#createModal').modal('hide');
-                },1000);
-                $('#tab_data').DataTable().ajax.reload();
-                toastr.success('Permission berhasil ditambahkan!', 'Success', {timeOut: 5000});
-              }
-            },
-            error:function(xhr) {
-              console.log(xhr);
-              $('#form_result').show();
-              html = '<div class="alert alert-danger">';
-              $.each(xhr.responseJSON.errors, function (key, item) {
-                html+='<p>' +item+'</p>';
-              });
-              html += '</div>';
-              $('#form_result').html(html);
-              $.each(xhr.responseJSON.errors,function(field_name,error){
-                  $(document).find('[name='+field_name+']').after('<span class="notifError text-strong text-danger"> <strong>' +error+ '</strong></span>');
-              });
-            }
-          });
-        }else{
-          $.ajax({
-            url:"{{route('per.update')}}",
-            method:"POST",
-            contentType: false,
-            cache:false,
-            processData: false,
-            dataType:"json",
-            data: new FormData(this),
-            success:function(data) {
-              // $('#form_result').show();
-              if(data.errors){
-                html = '<div class="alert alert-danger">';
-                for(var count = 0; count < data.errors.length; count++) {
-                  html += '<p>' + data.errors[count] + '</p>';
-                }
-                html += '</div>';
-                $('#form_result').html(html);
-              }
-              if(data.success) {
-                html = '<div class="alert alert-success">' + data.success + '</div>';
-                setTimeout(function(){
-                  $('#createModal').modal('hide');
-                },1000);
-                $('#tab_data').DataTable().ajax.reload();
-                toastr.success('Permission berhasil diperbarui!', 'Success', {timeOut: 5000});
-              }
-            },
-            error:function(xhr) {
-              console.log(xhr);
-              $('#form_result').show();
-              html = '<div class="alert alert-danger">';
-              $.each(xhr.responseJSON.errors, function (key, item) {
-                html+='<p>' +item+'</p>';
-              });
-              html += '</div>';
-              $('#form_result').html(html);
-            }//end error
-          });
+  $('#add').on('submit',function(event){
+    $('.notifError').remove();
+    event.preventDefault();
+    if($('#action').val() == 'tambah'){
+      $.ajax({
+        url:"{{route('permission.store')}}",
+        method:"POST",
+        contentType: false,
+        cache:false,
+        processData: false,
+        dataType:"json",
+        data: new FormData(this),
+        success:function(data) {
+          $('#form_result').hide();
+          if(data.success) {
+            html = '<div class="alert alert-success">' + data.success + '</div>';
+            setTimeout(function(){
+              $('#createModal').modal('hide');
+            },1000);
+            $('#tab_data').DataTable().ajax.reload();
+            toastr.success('Permission berhasil ditambahkan!', 'Success', {timeOut: 5000});
+          }
+        },
+      error:function(xhr) {
+        console.log(xhr);
+        $('#form_result').show();
+        html = '<div class="alert alert-danger">';
+        $.each(xhr.responseJSON.errors, function (key, item) {
+          html+='<p>' +item+'</p>';
+        });
+        html += '</div>';
+        $('#form_result').html(html);
+        $.each(xhr.responseJSON.errors,function(field_name,error){
+          $(document).find('[name='+field_name+']').after('<span class="notifError text-strong text-danger"> <strong>' +error+ '</strong></span>');
+        });
+      }
+    });
+  }else{
+    $.ajax({
+      url:"{{route('per.update')}}",
+      method:"POST",
+      contentType: false,
+      cache:false,
+      processData: false,
+      dataType:"json",
+      data: new FormData(this),
+      success:function(data) {
+        $('#form_result').hide();
+        if(data.success) {
+          html = '<div class="alert alert-success">' + data.success + '</div>';
+          setTimeout(function(){
+            $('#createModal').modal('hide');
+          },1000);
+          $('#tab_data').DataTable().ajax.reload();
+          toastr.success('Permission berhasil diperbarui!', 'Success', {timeOut: 5000});
         }
-      });
+      },
+      error:function(xhr) {
+        console.log(xhr);
+        $('#form_result').show();
+        html = '<div class="alert alert-danger">';
+        $.each(xhr.responseJSON.errors, function (key, item) {
+          html+='<p>' +item+'</p>';
+        });
+        html += '</div>';
+        $('#form_result').html(html);
+        $.each(xhr.responseJSON.errors,function(field_name,error){
+          $(document).find('[name='+field_name+']').after('<span class="notifError text-strong text-danger"> <strong>' +error+ '</strong></span>');
+        });
+      }//end error
+    });
+    }
   });
+});
 </script>
 @stop
