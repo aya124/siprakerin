518ca066c27542821b5d64cbbb5a8b3521c9ba88
aya124 <aya12496@gmail.com>
2021-07-10 11:28:08 +0700

    Update dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,52 +3,53 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+  <h1>Dashboard</h1>
 @stop
 
 @section('content')
-    <!-- <p><h4> Selamat Datang di SIPRAKERIN!</h4></p> -->
+  <!-- <p><h4> Selamat Datang di SIPRAKERIN!</h4></p> -->
 
-  <!-- <div class="callout callout-warning">
+  <div class="callout callout-warning">
     <h4>Attention, please!</h4>
     <b>Bagi anda yang telah berhasil login dan menggunakan menu serta fitur SIPrakerin, 
     kami minta tolong partisipasinya untuk mengisi kuesioner melalui link berikut.</b><br />
     <a href="https://forms.gle/yzcdkVaW8YSfWgUHA">Kuesioner Usability</a>
-  </div> -->
+  </div>
     
-    <p><h4>Petunjuk Penggunaan SIPrakerin</h4></p>
-    <div class="box box-widget">
-        <div class="box-header with-border">
-          <div class="user-block">
-            <span class="username">Menu Data Industri</span>
-            <!-- <span class="description">by Admin</span> -->
-          </div>
-          <!-- /.user-block -->
-          <div class="box-tools">
-            <button type="button" class="btn btn-box-tool" data-toggle="tooltip" title="" data-original-title="Mark as read">
-              <i class="fa fa-circle-o"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-          </div>
-          <!-- /.box-tools -->
-        </div>
-        <!-- /.box-header -->
-        <div class="box-body">
-        <p>Sebelum anda menggunakan menu-menu SIPrakerin, pastikan anda telah membaca dan memahami seluruh petunjuk di bawah ini.</p>
-        @role('siswa')
-        <p>Sebagai Siswa, anda dapat mengakses menu Data Industri, Pengajuan Prakerin, Laporan & Sertifikat serta Nilai.</p>
-        @endrole
-        @role('wali-kelas')
-        <p>Sebagai Wali Kelas, anda dapat mengakses menu Data Industri, Laporan & Sertifikat, Nilai, juga mencetak rekap data Pengajuan, Laporan & Sertifikat dan Nilai.</p>
-        @endrole
-        @role('kps')
-        <p>Sebagai Ketua Program Keahlian, anda dapat mengakses menu Data Industri, Validasi Pengajuan, Laporan & Sertifikat
-        Nilai, juga mencetak rekap data Pengajuan, Laporan & Sertifikat dan Nilai.</p>
-        @endrole
-        @role('admin')
-        <p>Sebagai Admin, anda memiliki kendali penuh terhadap semua menu utama yang terdapat pada SIPRAKERIN.</p>
-        @endrole
+  <p><h4>Petunjuk Penggunaan SIPrakerin</h4></p>
+  <div class="box box-widget">
+    <div class="box-header with-border">
+      <div class="user-block">
+        <span class="username">Menu Data Industri</span>
+        <!-- <span class="description">by Admin</span> -->
+      </div>
+      <!-- /.user-block -->
+      <div class="box-tools">
+        <button type="button" class="btn btn-box-tool" data-toggle="tooltip" title="" data-original-title="Mark as read">
+          <i class="fa fa-circle-o"></i></button>
+        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+        </button>
+        <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+      </div>
+      <!-- /.box-tools -->
+    </div>
+    <!-- /.box-header -->
+    <div class="box-body">
+      <p>Sebelum anda menggunakan menu-menu SIPrakerin, pastikan anda telah membaca dan memahami seluruh petunjuk di bawah ini.</p>
+          @role('siswa')
+          <p>Sebagai Siswa, anda dapat mengakses menu Data Industri, Pengajuan Prakerin, Laporan & Sertifikat serta Nilai.</p>
+          @endrole
+          @role('wali-kelas')
+          <p>Sebagai Wali Kelas, anda dapat mengakses menu Data Industri, Laporan & Sertifikat, Nilai, juga mencetak rekap data Pengajuan, Laporan & Sertifikat dan Nilai.</p>
+          @endrole
+          @role('kps')
+          <p>Sebagai Ketua Program Keahlian, anda dapat mengakses menu Data Industri, Validasi Pengajuan, Laporan & Sertifikat
+          Nilai, juga mencetak rekap data Pengajuan, Laporan & Sertifikat dan Nilai.</p>
+          @endrole
+          @role('admin')
+          <p>Sebagai Admin, anda memiliki akses pada 2 menu utama, yakni Data Industri dan Validasi Pengajuan. 
+          Di samping itu, anda juga berhak mengakses menu-menu pada bagian Management. </p>
+          @endrole
           <p>Pada menu Data Industri, anda dapat mengetahui industri mana saja yang bisa menjadi tempat dalam melaksanakan praktik kerja industri.
           @role(['wali-kelas','siswa'])
           <img class="img-responsive pad" src="../pictures/png/industry-view.png" alt="Industri">
@@ -56,8 +57,9 @@
           @role(['admin','kps'])
           <img class="img-responsive pad" src="../pictures/png/industry-view-admin.png" alt="Industry-View">
           @endrole
+
           <p>Untuk memudahkan pencarian, anda dapat menggunakan fitur <b><i>Search</i></b>.</p>
-          <img class="img-responsive pad" src="../pictures/png/industry-search.png" alt="Cari-Industri">
+          <img class="img-responsive pad" src="../pictures/png/industry-search.png" alt="Cari-Industri"> 
           <p>Anda dapat melihat masing-masing data industri lebih lengkap melalui tombol <b><i>Detail</i></b>.</p>
           <img class="img-responsive pad" src="../pictures/png/industry-detail.png" alt="Detail-Industri">
           <p>Anda berhak menambah data industri sesuai dengan informasi yang anda ketahui melalui tombol <b><u>Tambah Industri</u></b>. <br />
@@ -165,7 +167,7 @@
           <img class="img-responsive pad" src="../pictures/png/validation-approved.png" alt="Validasi-Disetujui">
           <p>Sedangkan, tabel terakhir berupa tabel daftar pengajuan yang telah ditolak.</p>
           <p>Anda dapat memperoleh keterangan atau info mengenai progress pengajuan yang dibuat siswa melalui tombol <b><i>Lihat Feedback</i></b>.</p>
-           <img class="img-responsive pad" src="../pictures/png/validation-lihat-feedback.png" alt="Lihat-Feedback">
+          <img class="img-responsive pad" src="../pictures/png/validation-lihat-feedback.png" alt="Lihat-Feedback">
           
         </div>
         <!-- /.box-body -->
@@ -378,17 +380,17 @@
         </div>
         <!-- /.box-header -->
         <div class="box-body">
-          <p>Pada bagian Management, anda berhak untuk melakukan manajemen (CRUD) pada menu User, Status, Role, dan Permission.</p>
+          <p>Pada bagian Management, anda berhak untuk melakukan manajemen (CRUD) pada menu User, Kelas, dan Status.</p>
           <p>Pada menu User, anda dapat meng-input user baru dengan ketentuan yang lebih spesifik.</p>
            <img class="img-responsive pad" src="../pictures/png/user-view.png" alt="User-View">
-          <p>Perhatikan tampilan berikut. Melalui menu ini, anda dapat mengaktifkan user dengan <i>Role</i> <b>"siswa"</b>.</p>
+          <p>Perhatikan tampilan berikut. Melalui menu ini, anda dapat mengaktifkan user yang memiliki <i>Role</i> sebagai <b>"siswa"</b>.</p>
           <img class="img-responsive pad" src="../pictures/jpg/unlocking-user.jpg" alt="User-Activation">
-          <p>Klik tombol <b><u>Aktifkan</u></b> agar user tersebut dapat membuat pengajuan lagi.</p>
+          <p>Klik tombol <b><i>Unlock</i></b> lalu klik <b>Aktifkan</b> agar user tersebut dapat membuat pengajuan lagi.</p>
           <img class="img-responsive pad" src="../pictures/png/user-unlock.png" alt="User-Activation-Confirm">
-          <!-- <p>Pada menu Kelas, anda dapat meng-input dan mengubah data kelas.</p> -->
+          <p>Pada menu Kelas, anda dapat menambah dan mengubah data kelas.</p>
           <p>Pada menu Status, anda dapat melakukan manajemen data status sebagai keterangan pada pengajuan prakerin.</p>
-          <p>Pada menu Role, anda dapat melakukan manajemen data role atau peran sesuai dengan kebutuhan.</p>
-          <p>Pada menu Permission, anda dapat melakukan manajemen data permission yang berupa tugas untuk masing-masing role.</p>
+          <!-- <p>Pada menu Role, anda dapat melakukan manajemen data role atau peran sesuai dengan kebutuhan.</p> -->
+          <!-- <p>Pada menu Permission, anda dapat melakukan manajemen data permission yang berupa tugas untuk masing-masing role.</p> -->
         </div>
         <!-- /.box-body -->
         <!-- /.box-footer -->
